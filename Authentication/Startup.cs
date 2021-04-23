9a57656537c3adb3c17a3c881c01298e755b50f5
Robert8034 <53059207+Robert8034@users.noreply.github.com>
2021-04-23 14:28:59 +0200

    Changed: - Shared is now a nuget package - All other projects access share via nuget instead of direct project reference - kubernetes config has been changed to experiment with exposing services

--- Authentication/Startup.cs
+++ Authentication/Startup.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Authentication.DAL;
 using Authentication.MessageHandlers;
 using Authentication.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Shared;
 
@@ -55,7 +48,7 @@ namespace Authentication
                 };
             }
             );
-
+            
             services.AddAuthorization();
 
             var connection = @"Server=authdb;Database=master;User=sa;Password=Your_password123;";
