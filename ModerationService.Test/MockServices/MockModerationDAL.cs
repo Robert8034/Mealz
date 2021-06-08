02bbd823d706fe1aade4aa9e438b7153a9ef4ed2
Robert8034 <53059207+Robert8034@users.noreply.github.com>
2021-06-08 15:48:55 +0200

    Changes: - Added unit testing for moderation service

---
+++ ModerationService.Test/MockServices/MockModerationDAL.cs
@@ -0,0 +1,60 @@
+﻿using ModerationService.DAL;
+using ModerationService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModerationService.Test.MockServices
+{
+    public class MockModerationDAL : IModerationDAL
+    {
+        public List<Report> reports;
+        public List<Request> requests;
+
+        public MockModerationDAL()
+        {
+            reports = new List<Report>();
+            requests = new List<Request>();
+        }
+
+        public List<Report> GetReports()
+        {
+            return reports;
+        }
+
+        public List<Report> GetReportsByReporterId(Guid id)
+        {
+            return reports.FindAll(e => e.ReporterId == id);
+        }
+
+        public List<Request> GetRequests()
+        {
+            return requests;
+        }
+
+        public Task PostReport(Report report)
+        {
+            reports.Add(report);
+            return Task.CompletedTask;
+        }
+
+        public Task PostRequest(Request request)
+        {
+            requests.Add(request);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveReport(Report report)
+        {
+            reports.Remove(report);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveRequest(Request request)
+        {
+            requests.Remove(request);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ ModerationService.Test/UnitTests/ModerationServiceTests.cs
@@ -0,0 +1,169 @@
+﻿using ModerationService.Test.MockServices;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ModerationService.Services;
+
+namespace ModerationService.Test.UnitTests
+{
+    [TestFixture]
+    public class ModerationServiceTests
+    {
+        [Test]
+        public void ApproveRequestSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var request = new Models.Request { RequestId = guid, RequestType = Models.RequestType.Upgrade_To_Chef, UserId = guid };
+
+            mockModerationDAL.requests.Add(request);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.ApproveRequest(request);
+
+            //Assert
+            Assert.AreEqual(0, mockModerationDAL.requests.Count);
+        }
+
+        [Test]
+        public void GetRequestsSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var request = new Models.Request { RequestId = guid, RequestType = Models.RequestType.Upgrade_To_Chef, UserId = guid };
+
+            mockModerationDAL.requests.Add(request);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.GetRequests();
+
+            //Assert
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [Test]
+        public void GetReportsSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var report = new Models.Report { PostId = guid, ReporterId = guid, ReportId = guid };
+
+            mockModerationDAL.reports.Add(report);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.GetReports();
+
+            //Assert
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [Test]
+        public void GetMyReportsSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var report = new Models.Report { PostId = guid, ReporterId = guid, ReportId = guid };
+
+            mockModerationDAL.reports.Add(report);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.GetMyReports(guid);
+
+            //Assert
+            Assert.AreEqual(result.Count, 1);
+        }
+
+        [Test]
+        public void PostReportSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var report = new Models.Report { PostId = guid, ReporterId = guid, ReportId = guid };
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.PostReport(report);
+
+            //Assert
+            Assert.AreEqual(mockModerationDAL.reports.Count, 1);
+        }
+
+        [Test]
+        public void PostRequestSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var request = new Models.Request { RequestId = guid, RequestType = Models.RequestType.Upgrade_To_Chef, UserId = guid };
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.PostRequest(request);
+
+            //Assert
+            Assert.AreEqual(mockModerationDAL.requests.Count, 1);
+        }
+
+        [Test]
+        public void DeclineRequestSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var request = new Models.Request { RequestId = guid, RequestType = Models.RequestType.Upgrade_To_Chef, UserId = guid };
+
+            mockModerationDAL.requests.Add(request);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.DeclineRequest(request);
+
+            //Assert
+            Assert.AreEqual(mockModerationDAL.requests.Count, 0);
+        }
+
+        [Test]
+        public void RemoveReportSuccess()
+        {
+            //Arrange
+            MockModerationDAL mockModerationDAL = new MockModerationDAL();
+            Guid guid = Guid.NewGuid();
+
+            var report = new Models.Report { PostId = guid, ReporterId = guid, ReportId = guid };
+
+            mockModerationDAL.reports.Add(report);
+
+            Services.ModerationService moderationService = new Services.ModerationService(mockModerationDAL);
+
+            //Act
+            var result = moderationService.RemoveReport(report);
+
+            //Assert
+            Assert.AreEqual(mockModerationDAL.reports.Count, 0);
+        }
+    }
+}
